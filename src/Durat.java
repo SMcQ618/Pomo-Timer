e597977909db839f015d92d75c39e5f1455ebda5
Stephen Sodipo <108195516+SMcQ618@users.noreply.github.com>
2024-07-13 16:06:30 -0400

    Figured out how I want the time to be set

--- src/Durat.java
+++ src/Durat.java
@@ -2,5 +2,25 @@
  * This class represents My way of how the user will be able to input time.
  */
 public class Durat {
+  private int hours;
+  private int minutes;
+  private int seconds;
 
+  public Durat(int hours, int minutes, int seconds) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+  }
+
+  public Durat(int minutes, int seconds) {
+    this(0, minutes, seconds);
+  }
+
+  public Durat(int seconds) {
+    this(0, 0, seconds);
+  }
+
+  public int getTotalSec() {
+    return hours * 3600 + minutes * 60 + seconds;
+  }
 }
